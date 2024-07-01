8980656506446b26559dd3413fe10ced544ae657
joelson Silva <joelson.s@outlook.com>
2024-07-01 00:23:05 -0300

    ajustando header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ const logo = "assets/images/logo.svg";
 function Header() {
   return (
     <>
-      <header className="z-20 bg-White p-4 shadow-md lg:px-8">
+      <header className="fixed z-20 w-full bg-White p-4 shadow-md lg:px-8">
         <div className="mx-auto flex items-center justify-between lg:max-w-[1440px]">
           <img src={logo} alt="easybank" />
           <Nav />
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,9 +3,9 @@ const heroImage = "assets/images/image-mockups.png";
 
 function Hero() {
   return (
-    <div className="z-0 h-screen overflow-hidden bg-VeryLightGray sm:pb-16">
-      <div className="bg-intro-mobile lg:bg-intro-desktop mx-auto lg:flex lg:items-center lg:justify-between lg:pl-8 xl:max-w-[1440px]">
-        <img src={heroImage} className="mx-auto -mt-28 grid lg:hidden" />
+    <div className="bg-intro-mobile lg:bg-intro-desktop z-0 h-screen overflow-hidden bg-VeryLightGray sm:pb-16">
+      <div className="mx-auto lg:flex lg:items-center lg:justify-between lg:pl-8 xl:max-w-[1440px]">
+        <img src={heroImage} className="mx-auto -mt-16 grid lg:hidden" />
         <div className="mt-8 text-center lg:-mt-8 lg:w-96 lg:text-left">
           <h1 className="text-4xl text-DarkBlue lg:text-5xl">
             Next generation digital banking
@@ -18,7 +18,7 @@ function Hero() {
         </div>
         <img
           src={heroImage}
-          className="xl:-mr-50 -mt-24 hidden lg:-mr-32 lg:block lg:w-[44rem]"
+          className="xl:-mr-50 -mt-10 hidden lg:-mr-32 lg:block lg:w-[44rem]"
         />
       </div>
     </div>
